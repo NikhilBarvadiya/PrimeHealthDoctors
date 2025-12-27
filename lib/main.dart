4b66ab0bf3823144f59c81e746c9e08bb08e8322
Nikhil Barvadiya <nikhilkumarbarvadiya@example.com>
2025-12-27 12:44:21 +0530

    offline ui added & auto-updater added...!

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_offline/flutter_offline.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:intl/date_symbol_data_local.dart';
@@ -14,6 +15,7 @@ import 'package:prime_health_doctors/utils/config/app_config.dart';
 import 'package:prime_health_doctors/utils/routes/route_methods.dart';
 import 'package:prime_health_doctors/utils/routes/route_name.dart';
 import 'package:prime_health_doctors/utils/theme/light.dart';
+import 'package:prime_health_doctors/views/no_internet.dart';
 import 'package:prime_health_doctors/views/preload.dart';
 import 'package:prime_health_doctors/views/restart.dart';
 import 'package:toastification/toastification.dart';
@@ -57,9 +59,20 @@ class _MyAppState extends State<MyApp> {
     return ToastificationWrapper(
       child: GetMaterialApp(
         builder: (BuildContext context, widget) {
-          return MediaQuery(
-            data: MediaQuery.of(context).copyWith(textScaler: const TextScaler.linear(1.0)),
-            child: widget!,
+          return OfflineBuilder(
+            connectivityBuilder: (BuildContext context, List<ConnectivityResult> connectivity, Widget child) {
+              if (connectivity.contains(ConnectivityResult.none)) {
+                return const NoInternet();
+              } else {
+                return child;
+              }
+            },
+            builder: (BuildContext context) {
+              return MediaQuery(
+                data: MediaQuery.of(context).copyWith(textScaler: const TextScaler.linear(1.0)),
+                child: widget!,
+              );
+            },
           );
         },
         title: AppConfig.appName,
--- lib/views/auth/splash/splash_ctrl.dart
+++ lib/views/auth/splash/splash_ctrl.dart
@@ -1,4 +1,5 @@
 import 'package:get/get.dart';
+import 'package:new_version_plus/new_version_plus.dart';
 import 'package:prime_health_doctors/utils/config/session.dart';
 import 'package:prime_health_doctors/utils/routes/route_name.dart';
 import 'package:prime_health_doctors/utils/storage.dart';
@@ -7,7 +8,28 @@ class SplashCtrl extends GetxController {
   @override
   void onInit() {
     super.onInit();
-    _navigateToHome();
+    verifyVersion();
+  }
+
+  void verifyVersion() async {
+    try {
+      NewVersionPlus newVersion = NewVersionPlus();
+      final status = await newVersion.getVersionStatus();
+      if (status != null && status.canUpdate) {
+        newVersion.showUpdateDialog(
+          context: Get.context!,
+          versionStatus: status,
+          dialogTitle: 'Update Available',
+          dialogText: 'A new version of the app is available. Please update to continue.',
+          updateButtonText: 'Update',
+          allowDismissal: false,
+        );
+      } else {
+        _navigateToHome();
+      }
+    } catch (e) {
+      _navigateToHome();
+    }
   }
 
   void _navigateToHome() async {
